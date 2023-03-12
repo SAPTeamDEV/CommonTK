15ecb02d226e780885e5419dd82e01a0e303bb99
AliP1986 <pa789892@gmail.com>
2023-03-12 20:21:21 +0330

    Make Config Class generic

---
+++ src/CommonTK/Config.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SAPTeam.CommonTK;
+
+internal class Config<T> : JsonWorker
+    where T : new()
+{
+    public T Prefs;
+
+    public Config(string configPath) : base(configPath)
+    {
+        if (!File.Exists(FileName))
+        {
+            Write(new T());
+        }
+        JObject cData = Open();
+        Prefs = Parse<T>(cData.CreateReader());
+    }
+
+    public void Write()
+    {
+        Write(Prefs);
+    }
+
+    private void Write(T cObject)
+    {
+        StringWriter sw = new();
+        JsonTextWriter jw = new(sw);
+        ToJson(jw, cObject);
+        Save(sw);
+    }
+}

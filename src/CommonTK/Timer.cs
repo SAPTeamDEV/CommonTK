1cae7a43507a5ed5ed7299a4860cb3423a565d28
AliP1986 <pa789892@gmail.com>
2023-03-13 01:15:50 +0330

    rewrite Timer class

--- src/CommonTK/Timer.cs
+++ src/CommonTK/Timer.cs
@@ -1,23 +1,46 @@
-﻿namespace SAPTeam.CommonTK;
+﻿using System.ComponentModel;
 
+namespace SAPTeam.CommonTK;
+
+/// <summary>
+/// A Timer that runs parameterless method after specified time in separate thread.
+/// </summary>
 public class Timer
 {
     private int delay;
-    private Action? callback;
+    private Action callback;
     private Thread? thread;
+    private bool repeat;
     private bool running;
+    private bool paused;
 
-    public static Timer Set(int msec, Action callback)
+    /// <summary>
+    /// Creates new <see cref="Timer"/> and starts it.
+    /// </summary>
+    /// <param name="msec">
+    /// Timer delay in Milliseconds.
+    /// </param>
+    /// <param name="callback">
+    /// A parameterless method or lambda expression that runs after passing <paramref name="msec"/>.
+    /// </param>
+    /// <param name="repeat">
+    /// Determines that <see cref="Timer"/> will be stopped after passing <paramref name="msec"/> or running until it stopped.
+    /// </param>
+    /// <returns>
+    /// An instance of <see cref="Timer"/> class.
+    /// </returns>
+    public static Timer Set(int msec, Action callback, bool repeat = false)
     {
         Timer timer = new()
         {
             delay = msec,
             callback = callback,
+            repeat = repeat,
             running = true
         };
         timer.thread = new Thread(timer.Run)
         {
-            Name = "Timer Thread"
+            Name = $"Timer {timer.GetHashCode()} Thread"
         };
         timer.thread.Start();
         return timer;
@@ -25,13 +48,69 @@ public class Timer
 
     private void Run()
     {
-        Thread.Sleep(delay);
-        if (!running) return;
-        callback();
+        while (true)
+        {
+            Thread.Sleep(delay);
+
+            if (!running)
+            {
+                break;
+            }
+
+            if (!paused)
+            {
+                callback();
+            }
+
+            if (!repeat)
+            {
+                break;
+            }
+        }
     }
 
-    public void Stop()
+    /// <summary>
+    /// Resumes timer if it already paused.
+    /// </summary>
+    public void Resume()
     {
-        running = false;
+        if (!running)
+        {
+            throw new InvalidOperationException("Timer is not running.");
+        }
+
+        if (!paused)
+        {
+            throw new InvalidOperationException("Timer is not paused.");
+        }
+
+        paused = false;
+    }
+
+    /// <summary>
+    /// Stops or Pauses the timer. Once a Timer has stopped, it can''t be restarted because the underlying <see cref="Thread"/> is died.
+    /// </summary>
+    /// <param name="pause">
+    /// Determines that this timer will be temporary paused or stopped.
+    /// </param>
+    public void Stop(bool pause = false)
+    {
+        if (!running)
+        {
+            throw new InvalidOperationException("Timer is not running.");
+        }
+
+        if (pause)
+        {
+            if (!paused)
+            {
+                throw new InvalidOperationException("Timer is already paused.");
+            }
+            paused = true;
+        }
+        else
+        {
+            running = false;
+        }
     }
 }

adead988049253b7f2acbc3fd124dd2879490b22
SAPTeamDEV <111853937+SAPTeamDEV@users.noreply.github.com>
2023-03-29 15:22:09 +0330

    Add missing API documentation entries for Config class

--- src/CommonTK/Config.cs
+++ src/CommonTK/Config.cs
@@ -5,21 +5,32 @@ using Newtonsoft.Json.Linq;
 
 namespace SAPTeam.CommonTK
 {
+    /// <summary>
+    /// Provides methods for create, use, change and save Application configuration files.
+    /// </summary>
+    /// <typeparam name="T">
+    /// A Deserializer class type that determines the configuration JSON file structure.
+    /// </typeparam>
     public class Config<T> : JsonWorker
         where T : new()
     {
+        /// <summary>
+        /// Gets an instance of <typeparamref name="T"/> that contains configuration values.
+        /// </summary>
         public T Prefs { get; }
 
-        public Config(string configPath) : base(configPath)
-        {
-            JObject cData = Open();
-            Prefs = Parse<T>(cData.CreateReader());
-        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Config{T}"/> class.
+        /// </summary>
+        /// <param name="configPath">
+        /// Path of configuration file, if the file is not exist it automatically created.
+        /// </param>
+        public Config(string configPath) : base(configPath) => Prefs = Parse<T>(Open().CreateReader());
 
-        public void Write()
-        {
-            Write(Prefs);
-        }
+        /// <summary>
+        /// Saves current values of <see cref="Prefs"/> properties to the configuration file.
+        /// </summary>
+        public void Write() => Write(Prefs);
 
         private void Write(T cObject)
         {

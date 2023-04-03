48c089a2494cdd4951659454f70dec4085eaeb1b
SAPTeamDEV <111853937+SAPTeamDEV@users.noreply.github.com>
2023-04-03 14:48:04 +0330

    Context: Add new overloads for HasContext and GetContext

--- CommonTK/ContextContainer.cs
+++ CommonTK/ContextContainer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace SAPTeam.CommonTK
 {
@@ -25,6 +26,34 @@ namespace SAPTeam.CommonTK
         }
 
         /// <summary>
+        /// Checks that the current session has the specified type of context.
+        /// </summary>
+        /// <param name="contextType">
+        /// A context class type.
+        /// </param>
+        /// <returns>
+        /// returns <see langword="true"/> if the current session has an instance of <paramref name="contextType"/>. otherwise it return <see langword="false"/>.
+        /// </returns>
+        public bool HasContext(Type contextType)
+        {
+            return GetContext(contextType) != null;
+        }
+
+        /// <summary>
+        /// Checks that the current session has the specified name of context.
+        /// </summary>
+        /// <param name="contextName">
+        /// Name of a context class.
+        /// </param>
+        /// <returns>
+        /// returns <see langword="true"/> if the current session has an instance of <paramref name="contextName"/>. otherwise it return <see langword="false"/>.
+        /// </returns>
+        public bool HasContext(string contextName)
+        {
+            return GetContext(contextName) != null;
+        }
+
+        /// <summary>
         /// Registers a new Context.
         /// </summary>
         /// <param name="context">
@@ -36,7 +65,7 @@ namespace SAPTeam.CommonTK
         }
 
         /// <summary>
-        /// Get the context object that maches with <typeparamref name="Context"/> type.
+        /// Gets the context object that matches with <typeparamref name="Context"/> name.
         /// </summary>
         /// <typeparam name="Context">
         /// A class type that implements the <see cref="Context"/> as base class.
@@ -47,12 +76,47 @@ namespace SAPTeam.CommonTK
         public Context GetContext<Context>()
             where Context : CommonTK.Context
         {
-            foreach (var context in contexts)
+            if (contexts.ContainsKey(typeof(Context).Name))
             {
-                if (context.Value is Context obj)
-                {
-                    return obj;
-                }
+                return (Context)contexts[typeof(Context).Name];
+            }
+
+            return default;
+        }
+
+        /// <summary>
+        /// Gets the context object that matches with <paramref name="contextType"/> type name.
+        /// </summary>
+        /// <param name="contextType">
+        /// A context class type.
+        /// </param>
+        /// <returns>
+        /// An existing instance of matching <paramref name="contextType"/> type. if there is no matching contexts it returns <see href="default"/>.
+        /// </returns>
+        public IContext GetContext(Type contextType)
+        {
+            if (contexts.ContainsKey(contextType.Name))
+            {
+                return contexts[contextType.Name];
+            }
+
+            return default;
+        }
+
+        /// <summary>
+        /// Gets the context object that matches with <paramref name="contextName"/> key.
+        /// </summary>
+        /// <param name="contextName">
+        /// Name of a context class.
+        /// </param>
+        /// <returns>
+        /// An existing instance of matching <paramref name="contextName"/> type name. if there is no matching contexts it returns <see href="default"/>.
+        /// </returns>
+        public IContext GetContext(string contextName)
+        {
+            if (contexts.ContainsKey(contextName))
+            {
+                return contexts[contextName];
             }
 
             return default;

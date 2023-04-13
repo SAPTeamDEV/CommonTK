a16bd39dc3113d6b9e4e1591439b55cd7edcee49
SAPTeamDEV <111853937+SAPTeamDEV@users.noreply.github.com>
2023-04-13 19:16:44 +0330

    Move action group feature methods to ActionGroup.cs

---
+++ CommonTK/ActionGroup.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SAPTeam.CommonTK
+{
+    public abstract partial class Context
+    {
+        static readonly Dictionary<string, ActionGroupContainer> groups = new Dictionary<string, ActionGroupContainer>();
+        static readonly object groupLockObj = new object();
+
+        /// <summary>
+        /// Queries all provided action groups and checks the locked state of each group.
+        /// If at least one of the specified action groups is locked, it throws a <see cref="ActionGroupException"/>.
+        /// </summary>
+        /// <param name="groups">
+        /// The name of action group or groups that will be queried.
+        /// </param>
+        /// <exception cref="ActionGroupException"></exception>
+        public static void QueryGroup(params string[] groups)
+        {
+            foreach (var group in groups)
+            {
+                if (Context.groups.ContainsKey(group) && Context.groups[group].IsLocked)
+                {
+                    if (Context.groups[group].Count == 1)
+                    {
+                        throw new ActionGroupException($"The action group \"{group}\" is locked by {Context.groups[group][0].Name} context.", ActionGroupError.Locked);
+                    }
+                    else
+                    {
+                        throw new ActionGroupException($"The action group \"{group}\" is locked by {Context.groups[group].Count} contexts.", ActionGroupError.Locked);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the state of the <paramref name="group"/>.
+        /// </summary>
+        /// <param name="group">
+        /// The name of the action group.
+        /// </param>
+        /// <returns>
+        /// A <see cref="ActionGroupState"/> represents the current state of the <paramref name="group"/>.
+        /// </returns>
+        public static ActionGroupState QueryGroupState(string group) => groups.ContainsKey(group) ? groups[group].State : ActionGroupState.Free;
+
+        /// <summary>
+        /// Generates a new action group.
+        /// </summary>
+        /// <param name="scope">
+        /// The scope of action group.
+        /// </param>
+        /// <param name="identifier">
+        /// The unique identifier of action group.
+        /// </param>
+        /// <param name="extras">
+        /// The extra identifier factors.
+        /// </param>
+        /// <returns>
+        /// The standardized action group name.
+        /// </returns>
+        public static string ActionGroup(ActionScope scope, string identifier, params string[] extras)
+        {
+            var groupName = string.Join(".", new string[] { scope.ToString(), identifier }.Concat(extras))
+                .Replace(' ', '_')
+                .ToLower();
+
+            return groupName;
+        }
+    }
+}
--- CommonTK/ContextManager.cs
+++ CommonTK/ContextManager.cs
@@ -10,8 +10,6 @@ namespace SAPTeam.CommonTK
         [DllImport("kernel32.dll")] private static extern IntPtr GetConsoleWindow();
 
         static readonly Dictionary<string, Context> contexts = new Dictionary<string, Context>();
-        static readonly Dictionary<string, ActionGroupContainer> groups = new Dictionary<string, ActionGroupContainer>();
-        static readonly object groupLockObj = new object();
         static readonly object contextLockObj = new object();
         private static InteractInterface interactinterface = GetConsoleWindow() != IntPtr.Zero ? InteractInterface.Console : InteractInterface.UI;
 
@@ -49,67 +47,6 @@ namespace SAPTeam.CommonTK
             }
 
             return context;
-        }
-
-        /// <summary>
-        /// Queries all provided action groups and checks the locked state of each group.
-        /// If at least one of the specified action groups is locked, it throws a <see cref="ActionGroupException"/>.
-        /// </summary>
-        /// <param name="groups">
-        /// The name of action group or groups that will be queried.
-        /// </param>
-        /// <exception cref="ActionGroupException"></exception>
-        public static void QueryGroup(params string[] groups)
-        {
-            foreach (var group in groups)
-            {
-                if (Context.groups.ContainsKey(group) && Context.groups[group].IsLocked)
-                {
-                    if (Context.groups[group].Count == 1)
-                    {
-                        throw new ActionGroupException($"The action group \"{group}\" is locked by {Context.groups[group][0].Name} context.", ActionGroupError.Locked);
-                    }
-                    else
-                    {
-                        throw new ActionGroupException($"The action group \"{group}\" is locked by {Context.groups[group].Count} contexts.", ActionGroupError.Locked);
-                    }
-                }
-            }
-        }
-
-        /// <summary>
-        /// Gets the state of the <paramref name="group"/>.
-        /// </summary>
-        /// <param name="group">
-        /// The name of the action group.
-        /// </param>
-        /// <returns>
-        /// A <see cref="ActionGroupState"/> represents the current state of the <paramref name="group"/>.
-        /// </returns>
-        public static ActionGroupState QueryGroupState(string group) => groups.ContainsKey(group) ? groups[group].State : ActionGroupState.Free;
-
-        /// <summary>
-        /// Generates a new action group.
-        /// </summary>
-        /// <param name="scope">
-        /// The scope of action group.
-        /// </param>
-        /// <param name="identifier">
-        /// The unique identifier of action group.
-        /// </param>
-        /// <param name="extras">
-        /// The extra identifier factors.
-        /// </param>
-        /// <returns>
-        /// The standardized action group name.
-        /// </returns>
-        public static string ActionGroup(ActionScope scope, string identifier, params string[] extras)
-        {
-            var groupName = string.Join(".", new string[] { scope.ToString(), identifier }.Concat(extras))
-                .Replace(' ', '_')
-                .ToLower();
-
-            return groupName;
         }
 
         /// <summary>

99cca6e655c9c7f4b602792296e031dc3431b07b
Alireza Poodineh <itsaeliux@gmail.com>
2025-05-20 19:58:05 +0330

    Use project root for storing python cache
    Signed-off-by: Alireza Poodineh <itsaeliux@gmail.com>

--- build.cmd
+++ build.cmd
@@ -1,8 +1,19 @@
 @echo off
 setlocal
 
-cd /d "%~dp0"
+SET "SCRIPT_DIR=%~dp0"
+cd /d "%SCRIPT_DIR%"
 
-python "%~dp0build.py" %*
+SET "CACHE_DIR=%SCRIPT_DIR%__pycache__"
+
+:: Create cache directory if it doesn't exist
+IF NOT EXIST "%CACHE_DIR%" (
+    mkdir "%CACHE_DIR%"
+)
+
+:: Set env var for this process and launch Python
+SET PYTHONPYCACHEPREFIX=%CACHE_DIR%
+
+python "%SCRIPT_DIR%build.py" %*
 
 endlocal
--- build.sh
+++ build.sh
@@ -3,4 +3,10 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR"
 
+CACHE_DIR="$SCRIPT_DIR/__pycache__"
+mkdir -p "$CACHE_DIR"
+
+# Tell Python to use that as the pycache prefix
+export PYTHONPYCACHEPREFIX="$CACHE_DIR"
+
 exec python3 "$SCRIPT_DIR/build.py" "$@"
